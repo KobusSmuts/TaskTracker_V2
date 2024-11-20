5be4abb234f3f8884968ad7d152ca54dcbdd3f1e
KobusSmuts <78231313+KobusSmuts@users.noreply.github.com>
2024-11-20 16:46:47 +0200

    Commit

--- app/src/main/java/com/example/tasktracker/RegisterActivity.java
+++ app/src/main/java/com/example/tasktracker/RegisterActivity.java
@@ -3,8 +3,10 @@ package com.example.tasktracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
-    private EditText etEmail, etCompanyName, etUsername, etPassword;
+    private EditText etEmail, etPassword;
+    private Spinner spnRole;
     private Button btnRegister, btnLogin;
     private FirebaseAuthService authService;
 
@@ -32,12 +35,17 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         etEmail = findViewById(R.id.etEmail);
-        etCompanyName = findViewById(R.id.etCompanyName);
-        etUsername = findViewById(R.id.etUsername);
+        spnRole = findViewById(R.id.spinner_register);
         etPassword = findViewById(R.id.etPassword);
         btnRegister = findViewById(R.id.btnRegister);
         btnLogin = findViewById(R.id.btnLogin);
         authService = new FirebaseAuthService();
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.register_role_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spnRole.setAdapter(adapter);
 
         btnRegister.setOnClickListener(view -> {
             String email = etEmail.getText().toString();
